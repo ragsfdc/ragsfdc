6ffe4caffc2cf484dea5a09e382939469e016e30
ragsfdc <91958666+ragsfdc@users.noreply.github.com>
2021-10-06 10:06:06 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print('hello rag')
+from tvDatafeed import TvDatafeed,Interval
+import os
+import datetime
+import logging
+import pandas as pd
+import numpy as np
+from selenium import webdriver
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
